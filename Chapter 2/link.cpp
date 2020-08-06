463f4e327730dd933e5c06d6ab07046be62c754a
Pranjal-dew <pranjal16100@iiitnr.edu.in>
2020-08-06 09:13:46 +0530

    chapter 2 commit

---
+++ Chapter 2/link.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+// Creating Data Type
+struct Node{
+	int data;
+	struct Node *next;
+};
+
+struct Node *head=NULL;// Declaring Head pointer
+
+void insert_end(int data)
+{
+	Node new_Node = new Node;// Declaring new_Node varible using user defined data type and putting values
+
+	new_Node->data=data;
+	new_Node->next=NULL;
+
+	if(head == NULL)
+		head=new_Node;
+	else
+		{
+			struct Node *ptr=head;
+			while(ptr!=NULL)
+				ptr=ptr->next;
+		}
+}
+
+void dis()
+{
+	if(head==NULL)
+		cout<<"List is empty.";
+	else
+	{	
+		struct Node *ptr=head;
+		while(ptr!=NULL)
+			{
+				cout<<ptr->data<<" ";
+				ptr=ptr->next;
+			}
+	}
+	cout<<endl;
+}
+
+int main()
+{
+	dis();
+	insert_end(1);
+	dis();
+	insert_end(2);
+	dis();
+	insert_end(3);
+	dis();
+	insert_end(4);
+	return 0;
+}
